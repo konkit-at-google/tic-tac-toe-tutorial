ce76e8ba89084eab30b6bfe8bee241d7d74ca835
konkit-at-google <139231501+konkit-at-google@users.noreply.github.com>
2023-09-18 10:41:36 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,109 @@
+/**
+Scenario
+
+* Create 9 buttons in 3 rows, with numbers as contents
+* Setting value as "X" on click
+* Toggling between "O" and "X"
+* Do not let clicks overwrite the fields
+* Add reset button
+* Add Win detection for one case
+* Add "game finished" flag
+* Add Win detection for all cases
+* (Optional) add draw detection
+* (Optional) beautify the app with CSS
+* (Optional) Modal instead of an alert
+**/
+
+sign = "X"
+gameFinished = false
+buttons = document.querySelectorAll("button")
+
+function setField(i) {
+  if (!gameFinished && notSet(i)) {
+    buttons[i].innerHTML = sign
+    
+    hasWon = checkIfFinished(sign);
+    if (hasWon) {
+        alert(sign + " has won")
+        gameFinished = true;
+        return
+    } 
+    
+    isDraw = checkDraw();
+    if (isDraw) {
+        alert("Draw")
+        gameFinished = true;
+        return
+    } 
+      
+    toggleSign()
+  }
+}
+
+function checkIfFinished(sign) {
+	if (getValue(0) == sign && getValue(1) == sign && getValue(2) == sign) {
+    return true
+  }
+  
+  if (getValue(3) == sign && getValue(4) == sign && getValue(5) == sign) {
+    return true
+  }
+  
+  if (getValue(6) == sign && getValue(7) == sign && getValue(8) == sign) {
+    return true
+  }
+  
+  if (getValue(0) == sign && getValue(3) == sign && getValue(6) == sign) {
+    return true
+  }
+  
+  if (getValue(1) == sign && getValue(4) == sign && getValue(7) == sign) {
+    return true
+  }
+  
+  if (getValue(2) == sign && getValue(5) == sign && getValue(8) == sign) {
+    return true
+  }
+  
+  if (getValue(0) == sign && getValue(4) == sign && getValue(8) == sign) {
+    return true
+  }
+  
+    if (getValue(2) == sign && getValue(4) == sign && getValue(6) == sign) {
+    return true
+  }
+  
+  return false
+}
+
+function checkDraw() {
+  for (i=0; i<9; i++) {
+    if (notSet(i)) {
+      return false;
+    }
+  }
+  return true
+}
+
+function getValue(i) {
+  return buttons[i].innerHTML
+}
+
+function notSet(i) {
+  return buttons[i].innerHTML != "X" && buttons[i].innerHTML != "O"
+}
+
+function toggleSign() {
+	if (sign == "X") {
+    sign = "O"
+  } else {
+    sign = "X"
+  }
+}
+
+function reset() {
+  for (i=0; i<9; i++) {
+    buttons[i].innerHTML = i
+  }
+  gameFinished = false;
+}
